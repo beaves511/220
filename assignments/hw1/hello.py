44ced044c7f38332e9dc007bd06ac0f5f0b875a4
Eric <beaverev@g.cofc.edu>
2021-09-03 12:39:28 -0400

    Completed hw 1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -2,7 +2,7 @@
 NAME: Eric Beaver
 hello.py
 
-Problem: this program print hello, world! to the screen
+Problem: This program prints hello, world! to the screen
 
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
