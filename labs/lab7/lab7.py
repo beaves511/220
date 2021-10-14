c60e2410dd2d1ddb8f8a242e393644e79c2b3579
Eric <beaverev@g.cofc.edu>
2021-10-14 14:58:40 -0400

    Completed lab 7

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -1,13 +1,76 @@
 """
-Name: <your name goes here – first and last>
-Partner: <your partner's name goes here – first and last>
-<ProgramName>.py
+Name: Eric Beaver
+Partner: Oliver Aschenbrenner
+lab7.py
 """
+import math
+from math import *
 
 
 def main():
-    # add function calls here
-    pass
+    cash_conversion()
+    encode()
+    print(sphere_area(3))
+    print(sphere_volume(3))
+    print(sum_n(5))
+    print(sum_n_cubes(3))
+    encode_better()
+
+
+def cash_conversion():
+    value = eval(input("Enter an integer: "))
+    print("${:.2f}".format(value))
+
+
+def encode():
+    message = input("Enter a message: ")
+    key = eval(input("Enter an integer key value: "))
+    acc = ""
+    for i in message:
+        num = ord(i)
+        add = num + key
+        cha = chr(add)
+        acc = acc + cha
+    print(acc)
+
+
+def sphere_area(radius):
+    surface_area = 4 * math.pi * math.pow(radius, 2)
+    return surface_area
+
+
+def sphere_volume(radius):
+    volume = (4 / 3) * math.pi * math.pow(radius, 3)
+    return volume
+
+
+def sum_n(n):
+    acc = 0
+    for i in range(n+1):
+        acc = acc + i
+    return acc
+
+
+def sum_n_cubes(n):
+    acc = 0
+    for i in range(n+1):
+        acc = acc + math.pow(i, 3)
+    return acc
+
+
+def encode_better():
+    message = input("Enter a message: ")
+    key_word = input("Enter a key: ")
+    acc = ""
+    for i in range(len(message)):
+        char = message[i]
+        key = key_word[i % len(key_word)]
+        char_val = ord(char)
+        key_val = ord(key) - 97
+        code = char_val + key_val
+        code_char = chr(code)
+        acc = acc + code_char
+    print(acc)
 
 
 main()
